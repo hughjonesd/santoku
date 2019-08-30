26e4834390ab042961a8071c6ce0d94342d35b52
David Hugh-Jones <davidhughjones@gmail.com>
2019-08-30 22:50:06 +0100

    More tests for breaks and chop

---
+++ tests/testthat/test-breaks.R
@@ -0,0 +1,68 @@
+
+test_that("brk_manual", {
+  for (l in c(TRUE, FALSE)) for (r in c(TRUE, FALSE)) {
+    expect_silent(x <- brk_manual(1:2, c(l, r)))
+    expect_s3_class(x, "breaks")
+  }
+
+  expect_error(brk_manual(c(2, 2), c(TRUE, TRUE)))
+  expect_error(brk_manual(c(2, 2), c(FALSE, TRUE)))
+  expect_error(brk_manual(c(2, 2), c(FALSE, FALSE)))
+  expect_silent(brk_manual(c(2, 2), c(TRUE, FALSE)))
+
+  expect_error(brk_manual(1, c(TRUE, FALSE)))
+  expect_error(brk_manual(1:2, c(TRUE)))
+  expect_error(brk_manual("a", TRUE))
+  expect_error(brk_manual(1, "c"))
+})
+
+
+test_that("brk_left, brk_right", {
+  expect_identical(
+          brk_left(1:3),
+          brk_manual(1:3, c(TRUE, TRUE, FALSE))
+        )
+  expect_identical(
+          brk_left(1:3, close_end = FALSE),
+          brk_manual(1:3, c(TRUE, TRUE, TRUE))
+        )
+  expect_identical(
+          brk_right(1:3),
+          brk_manual(1:3, c(TRUE, FALSE, FALSE))
+        )
+  expect_identical(
+          brk_right(1:3, close_end = FALSE),
+          brk_manual(1:3, c(FALSE, FALSE, FALSE))
+        )
+})
+
+
+test_that("brk_size", {
+  for (i in 1:10) {
+    x <- rnorm(sample(10:20, 1L))
+    b <- sample(5L, 1L)
+    expect_true(all(tab(!!x, brk_size(!!b), drop = TRUE) <= !!b),
+          info = sprintf("length(x) %s b %s", length(x), b))
+  }
+})
+
+
+test_that("brk_width", {
+  b <- brk_width(1)(0.5:1.5)
+  expect_equal(diff(as.vector(b)), 1)
+
+  width <- runif(1)
+  b <- brk_width(width)(0.5:1.5)
+  bvec <- as.vector(b)
+  expect_equal(diff(bvec)[1], width)
+  expect_equal(bvec[1], 0.5)
+
+  b <- brk_width(1)(rep(NA, 2))
+  expect_identical(as.vector(b), numeric(0))
+
+  b <- brk_width(1)(c(Inf, -Inf, NA))
+  expect_identical(as.vector(b), numeric(0))
+
+  b <- brk_width(1)(c(NA, 2, 4, NA))
+  expect_equal(diff(as.vector(b))[1], 1)
+})
--- tests/testthat/test-chop.R
+++ tests/testthat/test-chop.R
@@ -33,5 +33,39 @@ test_that("NA, NaN and Inf", {
   expect_equivalent(r, factor(c(NA, "a", "a"), levels = "a"))
   r <- chop(x, brk_left(Inf, close_end = FALSE), labels = "a")
   expect_equivalent(r, factor(c("a", "a", NA), levels = "a"))
+})
 
+
+test_that("extend", {
+  x <- c(1, 4)
+  expect_equivalent(
+          chop(x, 2:3, labels = lbl_numerals(), extend = TRUE),
+          factor(c(1, 3))
+        )
+  expect_equivalent(
+          chop(x, 2:3, labels = lbl_numerals(), extend = FALSE),
+          factor(c(NA, NA))
+        )
+})
+
+
+test_that("drop", {
+  x <- c(1, 3)
+  expect_equivalent(
+          levels(chop(x, 1:3, labels = lbl_numerals(), drop = TRUE)),
+          as.character(2:3)
+        )
+  expect_equivalent(
+          levels(chop(x, 1:3, labels = lbl_numerals(), drop = FALSE)),
+          as.character(1:4)
+        )
+})
+
+
+test_that("chop_quantiles", {
+  x <- 1:6
+  expect_equivalent(
+          chop_quantiles(x, c(.25, .5, .75)),
+          as.factor(c("0-25%", "0-25%", "25-50%", "50-75%", "75-100%", "75-100%"))
+        )
 })
