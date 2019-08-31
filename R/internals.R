abd8dfd13f30c36ffae5e52d1782fa978bd7fea9
David Hugh-Jones <davidhughjones@gmail.com>
2019-08-31 10:33:35 +0100

    remove old code

--- R/internals.R
+++ R/internals.R
@@ -21,27 +21,3 @@ singletons <- function (breaks) {
   dv <- diff(breaks)
   dv == 0L | is.nan(dv) # is.nan could be from Inf, Inf
 }
-
-
-old_categorize <- function (x, breaks) {
-  stopifnot(is.breaks(breaks))
-  stopifnot(is.numeric(x))
-
-  result <- rep(NA_integer_, length(x))
-
-  left <- attr(breaks, "left")
-  for (i in seq(1, length(breaks) - 1)) {
-    lb <- breaks[i]
-    rb <- breaks[i + 1]
-    lower_closed <- left[i]
-    upper_closed <- ! left[i + 1]
-    above_lower <- if(lower_closed) lb <= x else lb < x
-    below_upper <- if(upper_closed) x <= rb else x < rb
-    within <- above_lower & below_upper
-    # ! is.na ensures NAs stay as NA
-    result[within & ! is.na(within)] <- i
-  }
-
-  return(result)
-}
-
