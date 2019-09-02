5ec4696d68339baa952cf2647f92b7a762d6adcb
David Hugh-Jones <davidhughjones@gmail.com>
2019-09-02 16:27:34 +0100

    Add `exactly()`

--- R/internals.R
+++ R/internals.R
@@ -17,6 +17,21 @@ extend_breaks <- function (breaks) {
 }
 
 
+#' Syntactic sugar
+#'
+#' `exactly` lets you write `chop(x, c(1, exactly(2), 3))`. This
+#' is the same as `chop(x, c(1, 2, 2, 3))` but conveys your intent more
+#' clearly.
+#'
+#' @param x A numeric vector.
+#'
+#' @return `rep(x, each = 2)`.
+#' @export
+#'
+#' @examples
+exactly <- function (x) rep(x, each = 2)
+
+
 singletons <- function (breaks) {
   dv <- diff(breaks)
   dv == 0L | is.nan(dv) # is.nan could be from Inf, Inf
