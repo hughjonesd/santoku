20f4d545994ef13a708b983ad5c0d373eb2f3386
David Hugh-Jones <davidhughjones@gmail.com>
2019-09-03 17:16:09 +0100

    Two more tests

--- tests/testthat/test-labels.R
+++ tests/testthat/test-labels.R
@@ -8,6 +8,9 @@ test_that("sequential labels", {
   expect_equivalent(lbl_LETTERS()(brk), LETTERS[1:2])
 
   expect_equivalent(lbl_numerals("(%s)")(brk), sprintf("(%s)", 1:2))
+
+  expect_error(lbl_sequence(c("a", "a")))
+  expect_equivalent(lbl_sequence(letters[1:2])(brk), c("a", "b", "aa"))
 })
 
 
