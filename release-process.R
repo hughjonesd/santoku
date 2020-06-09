2bcdbf586a59fb53518ea2a78ae8cd584a7fc672
David Hugh-Jones <davidhughjones@gmail.com>
2020-06-09 09:58:40 +0100

    Move unnecessary files out of man/figures; other doc tweaks

--- release-process.R
+++ release-process.R
@@ -7,7 +7,16 @@ devtools::check_win_release()
 devtools::check()
 devtools::spell_check()
 pkgdown::build_site()
+
+my_home <- "~/hughjonesd.github.io/"
 rmarkdown::render("vignettes/tutorials/visual-introduction.Rmd",
-      output_dir = "~/hughjonesd.github.io/")
+      output_dir = my_home)
+file.copy("vignettes/tutorials/chopping-dates-with-santoku.Rmd", my_home,
+      recursive = TRUE)
+file.copy("vignettes/tutorials/figures", my_home)
+withr::with_dir(my_home,
+  rmarkdown::render("chopping-dates-with-santoku.Rmd")
+)
+
 devtools::release()
 # push hughjonesd.github.io
