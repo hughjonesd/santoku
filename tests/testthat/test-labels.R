a74c8fbd7ddb4bc45d58f8bfd7b770c3a75d32f6
David Hugh-Jones <davidhughjones@gmail.com>
2021-12-29 14:49:57 +0000

    Update tests for lbl_dash() and lbl_discrete()

--- tests/testthat/test-labels.R
+++ tests/testthat/test-labels.R
@@ -36,36 +36,34 @@ test_that("lbl_seq", {
 
 test_that("lbl_dash", {
   brk <- brk_res(brk_manual(1:3, rep(TRUE, 3)))
-  expect_equivalent(lbl_dash()(brk), c("1 - 2", "2 - 3"))
+  expect_equivalent(lbl_dash()(brk), c("1\u20142", "2\u20143"))
   expect_equivalent(lbl_dash("/")(brk), c("1/2", "2/3"))
 })
 
 
 test_that("lbl_dash arguments", {
   brk <- brk_res(brk_default(1:3), 1:2)
-  expect_equivalent(lbl_dash(fmt = "%.2f")(brk), c("1.00 - 2.00", "2.00 - 3.00"))
+  expect_equivalent(lbl_dash(fmt = "%.2f")(brk), c("1.00\u20142.00", "2.00\u20143.00"))
 
-  expect_equivalent(lbl_dash(first = "< 2")(brk), c("< 2", "2 - 3"))
-  expect_equivalent(lbl_dash(last = "> 2")(brk), c("1 - 2", "> 2"))
+  expect_equivalent(lbl_dash(first = "< 2")(brk), c("< 2", "2\u20143"))
+  expect_equivalent(lbl_dash(last = "> 2")(brk), c("1\u20142", "> 2"))
 
-  expect_equivalent(lbl_dash(first = "< %s")(brk), c("< 2", "2 - 3"))
-  expect_equivalent(lbl_dash(last = "> %s")(brk), c("1 - 2", "> 2"))
+  expect_equivalent(lbl_dash(first = "< %s")(brk), c("< 2", "2\u20143"))
+  expect_equivalent(lbl_dash(last = "> %s")(brk), c("1\u20142", "> 2"))
 
   qbrk <- brk_res(brk_quantiles(c(0, .5, 1)), x = 0:10)
-  expect_equivalent(lbl_dash()(qbrk), c("0% - 50%", "50% - 100%"))
-  expect_equivalent(lbl_dash(raw = TRUE)(qbrk), c("0 - 5", "5 - 10"))
+  expect_equivalent(lbl_dash()(qbrk), c("0%\u201450%", "50%\u2014100%"))
+  expect_equivalent(lbl_dash(raw = TRUE)(qbrk), c("0\u20145", "5\u201410"))
   expect_equivalent(
     lbl_dash(raw = TRUE, fmt = "%.2f")(qbrk),
-    c("0.00 - 5.00", "5.00 - 10.00")
+    c("0.00\u20145.00", "5.00\u201410.00")
   )
   expect_equivalent(
     lbl_dash(fmt = "%.3f")(qbrk),
-    c("0.000 - 0.500", "0.500 - 1.000")
+    c("0.000\u20140.500", "0.500\u20141.000")
   )
 
-
-
-  expect_equivalent(lbl_dash(fmt = brackets)(brk), c("(1) - (2)", "(2) - (3)"))
+  expect_equivalent(lbl_dash(fmt = brackets)(brk), c("(1)\u2014(2)", "(2)\u2014(3)"))
 })
 
 
@@ -82,7 +80,7 @@ test_that("lbl_format", {
     c("(1 - 2)", "(2 - 3)")
   )
 
-  brk2 <- brk_res(brk_left(c(1, 2, 2, 3)))
+  brk2 <- brk_res(brk_default(c(1, 2, 2, 3)))
   expect_equivalent(
     lbl_format("<%.1f to %.1f>", "|%.3f|")(brk2),
     c("<1.0 to 2.0>", "|2.000|", "<2.0 to 3.0>")
@@ -109,12 +107,12 @@ test_that("lbl_intervals", {
   expect_equivalent(lbl_intervals()(lbrk), c("[1, 2)", "[2, 3)"))
   expect_equivalent(lbl_intervals()(rbrk), c("(1, 2]", "(2, 3]"))
 
-  lbrk <- brk_res(brk_left(1:3), close_end = TRUE)
+  lbrk <- brk_res(brk_default(1:3), close_end = TRUE)
   expect_equivalent(lbl_intervals()(lbrk), c("[1, 2)", "[2, 3]"))
-  rbrk <- brk_res(brk_right(1:3), close_end = TRUE)
+  rbrk <- brk_res(brk_default(1:3), close_end = TRUE, left = FALSE)
   expect_equivalent(lbl_intervals()(rbrk), c("[1, 2]", "(2, 3]"))
 
-  sbrk <- brk_res(brk_left(c(1, 2, 2, 3)))
+  sbrk <- brk_res(brk_default(c(1, 2, 2, 3)))
   expect_equivalent(lbl_intervals()(sbrk), c("[1, 2)", "{2}", "(2, 3)"))
 
   mbrk <- brk_res(brk_manual(1:4, c(FALSE, TRUE, FALSE, TRUE)))
@@ -122,13 +120,13 @@ test_that("lbl_intervals", {
 })
 
 test_that("lbl_intervals arguments", {
-  lbrk <- brk_res(brk_left(c(1, 2, 2, 3) + 0.5))
+  lbrk <- brk_res(brk_default(c(1, 2, 2, 3) + 0.5))
   expect_equivalent(
     lbl_intervals(fmt = "%.2f")(lbrk),
     c("[1.50, 2.50)", "{2.50}",  "(2.50, 3.50)")
   )
 
-  lbrk <- brk_res(brk_left(1:3 * 10000))
+  lbrk <- brk_res(brk_default(1:3 * 10000))
   expect_equivalent(
     lbl_intervals(fmt = "%2g")(lbrk),
     c("[10000, 20000)", "[20000, 30000)")
@@ -166,11 +164,11 @@ test_that("lbl_discrete", {
   expect_equivalent(lbl_discrete()(rbrk), c("2", "3"))
 
   lbrk2 <- brk_res(brk_manual(c(1, 3, 5), rep(TRUE, 3)))
-  expect_equivalent(lbl_discrete()(lbrk2), c("1 - 2", "3 - 4"))
+  expect_equivalent(lbl_discrete()(lbrk2), c("1\u20142", "3\u20144"))
   expect_equivalent(lbl_discrete(" to ")(lbrk2), c("1 to 2", "3 to 4"))
 
-  lbrk3 <- brk_res(brk_left(c(1, 3, 3, 5)), close_end = TRUE)
-  expect_equivalent(lbl_discrete()(lbrk3), c("1 - 2", "3", "4 - 5"))
+  lbrk3 <- brk_res(brk_default(c(1, 3, 3, 5)), close_end = TRUE)
+  expect_equivalent(lbl_discrete()(lbrk3), c("1\u20142", "3", "4\u20145"))
 
   # break containing (1,2) which has no integer in it:
   open_brk <- brk_res(brk_manual(1:3, c(FALSE, TRUE, FALSE)))
@@ -183,22 +181,22 @@ test_that("lbl_discrete arguments", {
   lbrk <- brk_res(brk_default(c(1, 3, 5)))
   expect_equivalent(
     lbl_discrete(fmt = "(%s)")(lbrk),
-    c("(1) - (2)", "(3) - (4)")
+    c("(1)\u2014(2)", "(3)\u2014(4)")
   )
 
   expect_equivalent(
     lbl_discrete(fmt = brackets)(lbrk),
-    c("(1) - (2)", "(3) - (4)")
+    c("(1)\u2014(2)", "(3)\u2014(4)")
   )
 
   expect_equivalent(
     lbl_discrete(first = "<= %s")(lbrk),
-    c("<= 2", "3 - 4")
+    c("<= 2", "3\u20144")
   )
 
   expect_equivalent(
     lbl_discrete(last = ">= %s")(lbrk),
-    c("1 - 2", ">= 3")
+    c("1\u20142", ">= 3")
   )
 })
 
@@ -230,7 +228,7 @@ test_that("lbl_endpoint arguments", {
 
 
 test_that("bug: breaks labels don't produce duplicates", {
-  brk <- brk_res(brk_left(c(1.333333335, 1.333333336, 1.333333337, 5)))
+  brk <- brk_res(brk_default(c(1.333333335, 1.333333336, 1.333333337, 5)))
   lbls <- lbl_intervals()(brk)
   expect_equivalent(anyDuplicated(lbls), 0)
   lbls <- lbl_dash()(brk)
