e1e31a0f3ce8afa26414cce843a7d479ed60e0a0
David Hugh-Jones <davidhughjones@gmail.com>
2022-02-14 22:56:59 +0000

    Update docs.

--- R/non-standard-types-doc.R
+++ R/non-standard-types-doc.R
@@ -10,7 +10,7 @@
 #' * Character `x` and `breaks` are chopped with a warning.
 #' * If `x` and `breaks` are not the same type, they should be able to
 #'   be cast to the same type using [vctrs::vec_cast_common()].
-#' * Not all chopping operations make sense. For example, [chop_mean_sd()]
+#' * Not all chopping operations make sense, for example, [chop_mean_sd()]
 #'   on a character vector.
 #' * If you get errors, try setting `extend = FALSE`.
 #' * To request support for a type, open an issue on Github.
