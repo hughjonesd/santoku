7216682e779c70236b80e69d1f7caddb76d58353
David Hugh-Jones <davidhughjones@gmail.com>
2022-02-14 19:16:27 +0000

    Add pure R implementation of categorize() for objects that you can't make numeric

--- R/categorize.R
+++ R/categorize.R
@@ -14,10 +14,36 @@ categorize <- function (x, breaks) {
   # vec_cast won't accept e.g. characters but it also won't convert e.g. Dates
   # as.numeric accepts both
   # fuck you, world
-  x <- as.numeric(res[[1]])
-  breaks <- as.numeric(res[[2]])
+  x <- tryCatch(as.numeric(res[[1]]), error = function (...) res[[1]])
+  breaks <- tryCatch(as.numeric(res[[2]]), error = function (...) res[[2]])
 
-  codes <- categorize_impl(x, breaks, left)
+  codes <- if (is.numeric(x) && is.numeric(breaks)) {
+    categorize_impl(x, breaks, left)
+  } else {
+    categorize_non_numeric(x, breaks, left)
+  }
+
+  codes
+}
+
+
+categorize_non_numeric <- function (x, breaks, left) {
+  stopifnot(length(breaks) == length(left))
+
+  codes <- rep(NA_integer_, length(x))
+
+  for (i in seq_along(x)) {
+    x_i <- x[i]
+    for (j in seq_len(length(breaks) - 1)) {
+      lb <- breaks[j]
+      rb <- breaks[j + 1]
+      if ((x_i > lb || (left[j] && x_i == lb)) &&
+          (x_i < rb || (! left[j + 1] && x_i == rb))) {
+        codes[i] = j
+        break
+      }
+    }
+  }
 
   codes
 }
