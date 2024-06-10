1b0c6beaff7e6456836cc23a526a89417b5a7fc2
David Hugh-Jones <davidhughjones@gmail.com>
2024-06-10 17:25:15 +0100

    Fix test

--- tests/testthat/test-nonstandard.R
+++ tests/testthat/test-nonstandard.R
@@ -7,10 +7,10 @@ test_that("character", {
     chop(x, br)
   )
 
-  withr::with_options(list(santoku.warn_character = FALSE),
-    expect_silent(
-      chop(x, br)
-    )
+  withr::local_options(santoku.warn_character = FALSE)
+
+  expect_silent(
+    chop(x, br)
   )
 
   # here, we think there should *always* be a warning
